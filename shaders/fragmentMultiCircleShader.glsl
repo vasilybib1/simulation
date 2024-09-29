f0f332f1e09279da859964e86125e271726c1322
vasilybib1 <vasilyrubtsovru@gmail.com>
2024-09-29 00:50:51 -0700

    implemented verlet objects and added gravity

--- shaders/fragmentMultiCircleShader.glsl
+++ shaders/fragmentMultiCircleShader.glsl
@@ -2,18 +2,18 @@
 out vec4 FragColor;
 
 uniform vec2 u_resolution;
-uniform vec2 u_position_array[100];
-uniform float u_radius_array[100];
+uniform vec2 u_position_array[1];
+uniform float u_radius_array[1];
 
 void main(){
   vec4 color = vec4(0.0);
 
-  for(int i = 0; i < 100; ++i){
+  for(int i = 0; i < 1; ++i){
     vec2 st = gl_FragCoord.xy / u_resolution;
     float dist = distance(st, u_position_array[i]);
     
     if(dist < u_radius_array[i]){
-      color = vec4(u_position_array[i].x/2, u_position_array[i].y/2, 0.5, 1.0);
+      color = vec4(0.5 + u_position_array[i].x/4, 0.5 + u_position_array[i].y/4, 0.5, 1.0);
     }
   }
 
--- src/simulation.c
+++ src/simulation.c
@@ -11,15 +11,63 @@
 #define PI 3.14159265358979323846
 
 typedef struct {
-    float x;
-    float y;
+  float x;
+  float y;
 } vec2;
 
-const int WIDTH = 600;
-const int HEIGHT = 600;
-const int FPS = 60;
+typedef struct {
+  vec2 position;
+  vec2 positionLast;
+  vec2 acceleration;
+  float radius;
+} verletObj;
+
+const int WIDTH = 1000;
+const int HEIGHT = 1000;
+
+const vec2 GRAVITY = {0, -1.0f};
 
 int G_fill = 1;
+
+void verletObj_update(verletObj* obj, float dt){
+  vec2 displacement;
+  displacement.x = obj->position.x - obj->positionLast.x;
+  displacement.y = obj->position.y - obj->positionLast.y;
+  obj->positionLast = obj->position;
+  obj->position.x = obj->position.x + displacement.x + obj->acceleration.x * (dt * dt);
+  obj->position.y = obj->position.y + displacement.y + obj->acceleration.y * (dt * dt);
+}
+
+void verletObj_setVel(verletObj* obj, vec2 vel, float dt){
+  obj->positionLast.x = obj->position.x - (vel.x * dt);
+  obj->positionLast.y = obj->position.y - (vel.y * dt);
+}
+
+void verletObj_addVel(verletObj* obj, vec2 vel, float dt){
+  obj->positionLast.x -= vel.x * dt;
+  obj->positionLast.y -= vel.y * dt;
+}
+
+void verletObj_accelerate(verletObj* obj, vec2 a){
+  obj->acceleration.x += a.x;
+  obj->acceleration.y += a.y;
+}
+
+vec2 verletObj_getVelocity(verletObj obj, float dt){
+  return (vec2){(obj.position.x - obj.positionLast.x)/dt, (obj.position.y - obj.positionLast.y)/dt};
+}
+
+void sim_applyGravity(verletObj* obj, float dt, int size){
+  for(int i = 0; i < size; i++){
+    verletObj_accelerate(&obj[i], GRAVITY);
+  }
+}
+
+void sim_updatePosition(verletObj* obj, float dt, int size){
+  for(int i = 0; i < size; i++){
+    verletObj_update(&obj[i], dt);
+  }
+}
 
 void errorCallback(int error, const char* desc){ 
   fprintf(stderr, "Error: %s\n", desc);
@@ -38,6 +86,7 @@ static void keyCallBack(GLFWwindow* window, int key, int scancode, int action, i
 void resizeCallback(GLFWwindow* window, int width, int height){
   glViewport(0, 0, width, height);
 }
+
 
 int main(){
   glfwSetErrorCallback(errorCallback);
@@ -95,23 +144,17 @@ int main(){
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
-
-  // makes the circle arrays
-  vec2 positionArray[100];
-  float radiusArray[100];
-  for(int i = 0; i < 100; i++){
-    positionArray[i] = (vec2){
-      (float)rand()/RAND_MAX * 2.0f, 
-      (float)rand()/RAND_MAX * 2.0f
-    };
-    radiusArray[i] = 0.05f;
-    //printf("%f %f\n", positionArray[i].x, positionArray[i].y);
-  }
-
+  
+  verletObj cir[1];
+  cir[0].position = (vec2){1.0, 1.0};
+  cir[0].positionLast = (vec2){1.0, 1.0};
+  cir[0].acceleration = (vec2){0.0, 0.0};
+  cir[0].radius = 0.2f;
 
   // for fps calculation
   double lastTime = glfwGetTime();
   int nbFrames = 0;
+  float dt = 0.001f;
 
   while(!glfwWindowShouldClose(window)){
     // calculates ms per frame and prints the output once a second
@@ -123,7 +166,7 @@ int main(){
       lastTime += 1.0;
     }
     
-    // start of loop
+    // start of render
     glClear(GL_COLOR_BUFFER_BIT);
     
     setActiveShader(s);
@@ -132,16 +175,20 @@ int main(){
     glUniform2f(resolutionLoc, WIDTH, HEIGHT);
     
     int positionArrLoc = glGetUniformLocation(s->program, "u_position_array");
-    glUniform2fv(positionArrLoc, 100, (GLfloat *) positionArray);
+    glUniform2fv(positionArrLoc, 1, (GLfloat *) &cir->position);
     
     int radiusArrLoc = glGetUniformLocation(s->program, "u_radius_array");
-    glUniform1fv(radiusArrLoc, 100, radiusArray);
+    glUniform1fv(radiusArrLoc, 1, &cir->radius);
     
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     
     glfwSwapBuffers(window);
     glfwPollEvents();
+    // end of render
+    
+    sim_updatePosition(cir, dt, 1);
+    sim_applyGravity(cir, dt, 1);
   }
 
   glDeleteVertexArrays(1, &VAO);
